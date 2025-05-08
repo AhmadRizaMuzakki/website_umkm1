86b7a4ed7ac3bf2f8d814b5275bbc14eed8ef14c
Muhammadivnufajar04 <fajarribnu55@gmail.com>
2025-05-08 12:10:46 +0700

    menambahkan file login

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
+  <link rel="stylesheet" href="style.css"> <!-- Menghubungkan CSS eksternal -->
+</head>
+<body>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form>
+      <input type="text" placeholder="Username" required>
+      <input type="password" placeholder="Password" required>
+      <button type="submit">Masuk</button>
+    </form>
+    <div class="link">
+      <a href="#">Lupa password?</a>
+    </div>
+  </div>
+</body>
+</html>
